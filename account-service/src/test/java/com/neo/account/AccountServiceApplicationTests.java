404bd3bb814d9d9b505150dd499f1af1b375da20
Emmanuel Abodesegun <abodesegun.emmanuel@palmpay-inc.com>
2025-03-05 22:25:11 +0100

    added account service and transaction service

--- account-service/src/test/java/com/neo/account/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/neo/account/AccountServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.neo;
+package com.neo.account;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NeoBankingApplicationTests {
+class AccountServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
